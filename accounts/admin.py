af7a29560f57f5e276968e327b6b780a4bb3e38e
Roozbeho <roozbehtavanaee@gmail.com>
2025-02-12 18:27:06 +0330

    feat: Implement users Roles

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,38 @@
 from django.contrib import admin
 
-from .models import CustomUser
+from .models import CustomUser, Role, Permission
 
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'is_admin', 'verified', 'is_premium']
+    list_display = ['username', 'email', 'get_role', 'verified', 'is_premium']
     list_filter = ['joined_at', 'is_admin', 'is_premium', 'verified']
     show_facets = admin.ShowFacets.ALLOW
     list_per_page = 50
 
+    @admin.display(description='Role')
+    def get_role(self, obj):
+        return str(obj.role).split()[0]
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'get_permissions']
+
+    @admin.display(description='Permissions')
+    def get_permissions(self, obj):
+        roles = {
+            'User': [Permission.LIKE, Permission.COMMENT],
+            'PremiumUser': [Permission.FOLLOW, Permission.LIKE, Permission.BOOKMARK,
+                            Permission.COMMENT, Permission.WRITE],
+            'Moderator': [Permission.MODERATE_COMMENTS, Permission.COMMENT,
+                         Permission.EDIT_ARTICLE, Permission.DELETE_ARTICLE],
+            'Administrator': [Permission.ADMIN, Permission.FOLLOW, Permission.LIKE, 
+                              Permission.BOOKMARK, Permission.COMMENT],
+        }
+        permissions = []
+        for perm in roles[obj.name]:
+            permissions.append(Permission.get_name(perm))
+        
+        return "-".join(permissions)
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from random import randint
+from hashlib import sha256
 
 from django.db import models
 from django.core.cache import cache
@@ -8,6 +9,78 @@ from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
 
+class Permission:
+    FOLLOW = 1
+    LIKE = 2
+    BOOKMARK = 4
+    COMMENT = 8
+    WRITE = 16
+    EDIT_ARTICLE = 32
+    DELETE_ARTICLE = 64
+    MODERATE_COMMENTS = 128
+    ADMIN = 256
+
+    @classmethod
+    def get_name(cls, val):
+        return {value:key \
+                for key, value in dict(vars(Permission)).items() if isinstance(value, int)
+            }.get(val, None)
+
+class Role(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    default = models.BooleanField(default=False)
+    permissions = models.IntegerField(default=0)
+
+    @classmethod
+    def insert_roles(cls):
+        roles = {
+            'User': [Permission.LIKE, Permission.COMMENT],  # Basic users
+            'PremiumUser': [Permission.FOLLOW, Permission.LIKE, Permission.BOOKMARK,
+                            Permission.COMMENT, Permission.WRITE],  # Writers
+            'Moderator': [Permission.MODERATE_COMMENTS, Permission.COMMENT,
+                         Permission.EDIT_ARTICLE, Permission.DELETE_ARTICLE],  # Content Moderators
+            'Administrator': [Permission.ADMIN, Permission.FOLLOW, Permission.LIKE, 
+                              Permission.BOOKMARK, Permission.COMMENT],  # Manages users
+        }
+        default_role = 'User'
+        for role_name, perms in roles.items():
+            role = cls.objects.filter(name=role_name)
+
+            if not role.exists():
+                role = cls.objects.create(name=role_name, default = (role_name == default_role))
+            else:
+                role = role.first()
+
+            role.reset_permission()
+            for perm in perms:
+                role.add_permission(perm)
+
+            role.save()
+
+    @classmethod
+    def get_default_role_pk(cls):
+        role, created = cls.objects.get_or_create(
+            name='User', defaults={'permissions': Permission.LIKE | Permission.COMMENT}
+        )
+        return role.pk
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+    
+    def reset_permission(self):
+        self.permissions = 0
+    
+    def has_permission(self, perm):
+        return (self.permissions & perm) == perm
+    
+    def __str__(self):
+        return f'{self.name} (permissions: {self.permissions})'
+      
 class CustomUser(AbstractUser):
     email = models.EmailField(verbose_name=_('Email Address'), unique=True)
     username = models.CharField(verbose_name=_('User Name'), max_length=50, unique=True)
@@ -17,6 +90,8 @@ class CustomUser(AbstractUser):
     is_admin = models.BooleanField(verbose_name=_('Is Admin'), default=False)
     verified = models.BooleanField(verbose_name=_('Is Verified'), default=False)
     is_premium = models.BooleanField(verbose_name=_('Is Premium'), default=False)
+    role = models.ForeignKey(Role, default=Role.get_default_role_pk, null=True,
+                             on_delete=models.SET_NULL, related_name='users')
 
     joined_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
