aa22b992061e7d825a8a6d21ba31e540e0c0923b
Roozbeho <roozbehtavanaee@gmail.com>
2025-02-14 12:23:08 +0330

    feat: Implement custom pagination for posts

---
+++ blog/pagination.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+DEFAULT_PAGE = 25
+class PostListPagination(PageNumberPagination):
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    page_size = 25
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links':{
+                'next': self.get_next_link(),
+                'prev': self.get_previous_link()
+            },
+            'total': self.page.paginator.count,
+            'result': data
+        })
