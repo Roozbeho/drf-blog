c91bc4a18f1deee4c382e197c6e201c80835feca
Roozbeho <roozbehtavanaee@gmail.com>
2025-02-15 19:44:49 +0330

    feat: add CanUserWriteComment to permissions

--- blog/permissions.py
+++ blog/permissions.py
@@ -11,3 +11,11 @@ class OwnerAndAdminOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
         return obj.author == user or user.can(Permission.ADMIN)
+    
+class CanUserWriteComment(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.can(Permission.COMMENT) or request.user.can(Permission.ADMIN)
+    
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return (user.can(Permission.COMMENT) and obj.user == user) or user.can(Permission.ADMIN)
